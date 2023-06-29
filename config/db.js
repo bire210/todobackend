e850fb78dec13d37ea5de411f497668443d0eaeb
Birendra <divyabiru27@gmail.com>
2023-06-29 16:47:04 +0530

    modification

--- config/db.js
+++ config/db.js
@@ -1,7 +1,5 @@
 const mongoose=require("mongoose");
 require("dotenv").config();
-
 mongoose.set('strictQuery',false);
-
-const connecttion=mongoose.connect(process.env.mongoUrl);
+const connecttion=mongoose.connect(process.env.mongourl);
 module.exports=connecttion
